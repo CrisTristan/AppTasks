07bb2cc2fe80ac64c60312822318d88fc8877bc5
Cristian Abad <112132709+CrisTristan@users.noreply.github.com>
2023-08-27 11:55:16 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -4,7 +4,7 @@ export const pool = createPool({
     host: 'sql10.freesqldatabase.com',
     port: 3306,
     user: 'sql10642687',
-    password: bdaEXyxvKi,
+    password: 'bdaEXyxvKi',
     database: 'sql10642687'
 })
 
