6b03b202183b6cf819bf673fcb07b9b48af3e998
Cristian Abad <112132709+CrisTristan@users.noreply.github.com>
2023-08-29 21:16:57 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,11 +1,11 @@
 import { createPool } from 'mysql2/promise'
 import {DB_PASSWORD} from './config.js'
 export const pool = createPool({
-    host: 'sql10.freesqldatabase.com',
+    host: 'aws.connect.psdb.cloud',
     port: 3306,
-    user: 'sql10642687',
+    user: '44tsp5zyb4gutrjj2u7k',
     password: DB_PASSWORD,
-    database: 'sql10642687'
+    database: 'tasksdb'
 })
 
 
