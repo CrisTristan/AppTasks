00d46f299b44ecd9f4f111c84485df7d23a0947e
Cristian Abad <112132709+CrisTristan@users.noreply.github.com>
2023-08-29 21:18:07 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ import {DB_PASSWORD} from './config.js'
 export const pool = createPool({
     host: 'aws.connect.psdb.cloud',
     port: 3306,
-    user: '44tsp5zyb4gutrjj2u7k',
+    user: '89ij67sf2q63bf5j5605',
     password: DB_PASSWORD,
     database: 'tasksdb'
 })
