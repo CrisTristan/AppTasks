399bdc4afe8ba778fcbabe33b80bf08a7956208a
Cristian Abad <112132709+CrisTristan@users.noreply.github.com>
2023-08-30 19:39:41 -0500

    Update config.js

--- server/config.js
+++ server/config.js
@@ -1,2 +1,2 @@
 export const PORT = process.env.PORT || 4000;
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'pscale_pw_O6MLNdIIZ5FasxdH8mCcxMozaigAKN47tMupdE4Xqaq';
+export const DB_PASSWORD = process.env.DB_PASSWORD
